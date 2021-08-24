c2a7875d754a94cf638e22041c110b7f9f166e0c
Muhammed Mustafa <muhammed.muhammed.ext@siemens.com>
2021-08-24 16:59:13 +0200

    displayed a list of heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from 'src/mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,11 +8,14 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  };
+  heroes: Hero[] = HEROES;
+  selectedHero!: Hero;
   constructor() {}
 
   ngOnInit(): void {}
+
+  //METHODS
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
+  }
 }
